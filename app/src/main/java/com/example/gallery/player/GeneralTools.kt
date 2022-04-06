b5d3e000d3bca0478eb4b80dca5cd32f25dc171e
saidim <1466913327@qq.com>
2022-04-06 18:16:19 +0800

    add VerticalProgressView

--- app/src/main/java/com/example/gallery/player/GeneralTools.kt
+++ app/src/main/java/com/example/gallery/player/GeneralTools.kt
@@ -1,5 +1,6 @@
 package com.example.gallery.player
 
+import com.blankj.utilcode.util.Utils
 import kotlin.math.absoluteValue
 
 object GeneralTools {
@@ -7,5 +8,16 @@ object GeneralTools {
         val minute = (millisecond.absoluteValue.toLong() / 60000).toString().let { if (it.count() == 1) "0$it" else it }
         val second = (millisecond.absoluteValue.toLong() % 60000 / 1000).toString().let { if (it.count() == 1) "0$it" else it }
         return if (hasSpace) "$minute : $second" else "$minute:$second"
+    }
+
+    inline val Double.dp: Int get() = run {
+        return toFloat().dp
+    }
+    inline val Int.dp: Int get() = run {
+        return toFloat().dp
+    }
+    inline val Float.dp: Int get() = run {
+        val scale: Float = Utils.getApp().resources.displayMetrics.density
+        return (this * scale + 0.5f).toInt()
     }
 }
--- app/src/main/java/com/example/gallery/player/VerticalProgressView.kt
+++ app/src/main/java/com/example/gallery/player/VerticalProgressView.kt
@@ -1,8 +1,13 @@
 package com.example.gallery.player
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import com.example.gallery.player.GeneralTools.dp
+import org.jetbrains.anko.verticalPadding
 
 class VerticalProgressView : View {
     constructor(context: Context) : super(context)
@@ -13,5 +18,51 @@ class VerticalProgressView : View {
         defStyleAttrs
     )
 
+    private var backColor: Int = Color.GRAY
 
+    private var progressColor: Int = Color.WHITE
+
+    var defaultWidth = 4.dp
+
+    var defaultHeight = 100.dp
+
+    private val paint = Paint().apply {
+        strokeWidth = width.toFloat()
+        isAntiAlias = true
+        strokeCap = Paint.Cap.ROUND
+        style=Paint.Style.STROKE
+    }
+
+    var progress = 0
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        val paintPadding = width.toFloat() / 2
+        paint.color = backColor
+        paint.strokeWidth = width.toFloat()
+        canvas?.drawLine(paintPadding, paintPadding, paintPadding, height.toFloat() - width, paint)
+        paint.color = progressColor
+        canvas?.drawLine(paintPadding, height.toFloat() - width, paintPadding, (height - paintPadding) * (100 - progress) / 100, paint)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val with = when (MeasureSpec.getMode(widthMeasureSpec)) {
+            MeasureSpec.UNSPECIFIED -> defaultWidth
+            MeasureSpec.AT_MOST -> defaultWidth
+            MeasureSpec.EXACTLY -> MeasureSpec.getSize(widthMeasureSpec)
+            else -> defaultWidth
+        }
+        val height = when (MeasureSpec.getMode(heightMeasureSpec)) {
+            MeasureSpec.UNSPECIFIED -> defaultHeight
+            MeasureSpec.AT_MOST -> defaultHeight
+            MeasureSpec.EXACTLY -> MeasureSpec.getSize(heightMeasureSpec)
+            else -> defaultHeight
+        }
+        setMeasuredDimension(with, height)
+    }
 }
