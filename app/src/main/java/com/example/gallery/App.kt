85086cd3b007b3f01e400a21c98b1fd8cd105be2
saidim <1466913327@qq.com>
2023-05-15 11:29:34 +0800

    add SQLiteStudioRemote

--- app/src/main/java/com/example/gallery/App.kt
+++ app/src/main/java/com/example/gallery/App.kt
@@ -3,11 +3,16 @@ package com.example.gallery
 import com.blankj.utilcode.util.Utils
 import com.example.gallery.base.BaseApplication
 import com.facebook.drawee.backends.pipeline.Fresco
+import pl.com.salsoft.sqlitestudioremote.SQLiteStudioService
 
 class App : BaseApplication() {
     override fun onCreate() {
         super.onCreate()
         Utils.init(this)
         Fresco.initialize(this)
+
+        // start SQLiteStudio Service
+        SQLiteStudioService.instance().setPort(20009)
+        SQLiteStudioService.instance().start(this)
     }
 }
