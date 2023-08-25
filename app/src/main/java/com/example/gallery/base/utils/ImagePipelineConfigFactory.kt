6fddad48306865c610f219bdff58942de99674e6
saidim <1466913327@qq.com>
2023-08-25 17:06:35 +0800

    music: java -> kotlin

--- app/src/main/java/com/example/gallery/base/utils/ImagePipelineConfigFactory.kt
+++ app/src/main/java/com/example/gallery/base/utils/ImagePipelineConfigFactory.kt
@@ -1,105 +1,98 @@
-package com.example.gallery.base.utils;
+package com.example.gallery.base.utils
 
-import android.content.Context;
-import android.os.Build;
-import android.os.Environment;
-
-import com.blankj.utilcode.util.Utils;
-import com.example.gallery.App;
-import com.facebook.cache.disk.DiskCacheConfig;
-import com.facebook.common.internal.Supplier;
-import com.facebook.common.util.ByteConstants;
-import com.facebook.imagepipeline.cache.MemoryCacheParams;
-import com.facebook.imagepipeline.core.ImagePipelineConfig;
-import com.facebook.imagepipeline.decoder.ProgressiveJpegConfig;
-import com.facebook.imagepipeline.image.ImmutableQualityInfo;
-import com.facebook.imagepipeline.image.QualityInfo;
-
-import java.io.File;
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Build
+import android.os.Environment
+import com.blankj.utilcode.util.Utils
+import com.facebook.cache.disk.DiskCacheConfig
+import com.facebook.common.util.ByteConstants
+import com.facebook.imagepipeline.cache.MemoryCacheParams
+import com.facebook.imagepipeline.core.ImagePipelineConfig
+import com.facebook.imagepipeline.decoder.ProgressiveJpegConfig
+import com.facebook.imagepipeline.image.ImmutableQualityInfo
+import com.facebook.imagepipeline.image.QualityInfo
+import java.io.File
 
 /**
  * Creates ImagePipeline configuration for the sample app
  */
-public class ImagePipelineConfigFactory {
-    private static final String IMAGE_PIPELINE_CACHE_DIR = "imagepipeline_cache";
+@SuppressLint("StaticFieldLeak")
+object ImagePipelineConfigFactory {
+    private const val IMAGE_PIPELINE_CACHE_DIR = "imagepipeline_cache"
+    private var sImagePipelineConfig: ImagePipelineConfig? = null
 
-    private static ImagePipelineConfig sImagePipelineConfig;
-//    private static ImagePipelineConfig sOkHttpImagePipelineConfig;
-
-    private static final int MAX_HEAP_SIZE = (int) Runtime.getRuntime().maxMemory();
-
-    public static final int MAX_DISK_CACHE_SIZE = 300 * ByteConstants.MB;
-    public static final int MAX_MEMORY_CACHE_SIZE = MAX_HEAP_SIZE / 3;
+    //    private static ImagePipelineConfig sOkHttpImagePipelineConfig;
+    private val MAX_HEAP_SIZE = Runtime.getRuntime().maxMemory().toInt()
+    const val MAX_DISK_CACHE_SIZE = 300 * ByteConstants.MB
+    val MAX_MEMORY_CACHE_SIZE = MAX_HEAP_SIZE / 3
 
     /**
      * Creates config using android http stack as network backend.
      */
-    public static ImagePipelineConfig getImagePipelineConfig(Context context) {
+    fun getImagePipelineConfig(context: Context): ImagePipelineConfig? {
         if (sImagePipelineConfig == null) {
-            ImagePipelineConfig.Builder configBuilder = ImagePipelineConfig.newBuilder(context);
-            configureCaches(configBuilder, context);
-            setProgressive(configBuilder);
-            sImagePipelineConfig = configBuilder.build();
+            val configBuilder = ImagePipelineConfig.newBuilder(context)
+            configureCaches(configBuilder, context)
+            setProgressive(configBuilder)
+            sImagePipelineConfig = configBuilder.build()
         }
-        return sImagePipelineConfig;
+        return sImagePipelineConfig
     }
 
-    private static void setProgressive(ImagePipelineConfig.Builder configBuilder) {
-        ProgressiveJpegConfig progressiveJpegConfig = new ProgressiveJpegConfig() {
-            @Override
-            public int getNextScanNumberToDecode(int scanNumber) {
+    private fun setProgressive(configBuilder: ImagePipelineConfig.Builder) {
+        val progressiveJpegConfig: ProgressiveJpegConfig = object : ProgressiveJpegConfig {
+            override fun getNextScanNumberToDecode(scanNumber: Int): Int {
                 //返回下一个需要解码的扫描次数
-                return scanNumber + 2;
+                return scanNumber + 2
             }
 
-            public QualityInfo getQualityInfo(int scanNumber) {
-                boolean isGoodEnough = (scanNumber >= 5);
+            override fun getQualityInfo(scanNumber: Int): QualityInfo {
+                val isGoodEnough = scanNumber >= 5
                 //确定多少个扫描次数之后的图片才能开始显示。
-                return ImmutableQualityInfo.of(scanNumber, isGoodEnough, false);
+                return ImmutableQualityInfo.of(scanNumber, isGoodEnough, false)
             }
-        };
-//具体含义可参考 http://wiki.jikexueyuan.com/project/fresco/progressive-jpegs.html
-        configBuilder.setProgressiveJpegConfig(progressiveJpegConfig);
+        }
+        //具体含义可参考 http://wiki.jikexueyuan.com/project/fresco/progressive-jpegs.html
+        configBuilder.setProgressiveJpegConfig(progressiveJpegConfig)
     }
 
     /**
      * Configures disk and memory cache not to exceed common limits
      */
-    private static void configureCaches(ImagePipelineConfig.Builder configBuilder, Context context) {
-        final MemoryCacheParams bitmapCacheParams = new MemoryCacheParams(
-                MAX_MEMORY_CACHE_SIZE, // Max total size of elements in the cache
-                Integer.MAX_VALUE,                     // Max entries in the cache
-                MAX_MEMORY_CACHE_SIZE, // Max total size of elements in eviction queue
-                Integer.MAX_VALUE,                     // Max length of eviction queue
-                Integer.MAX_VALUE);                    // Max cache entry size
+    private fun configureCaches(configBuilder: ImagePipelineConfig.Builder, context: Context) {
+        val bitmapCacheParams = MemoryCacheParams(
+            MAX_MEMORY_CACHE_SIZE, Int.MAX_VALUE,  // Max entries in the cache
+            MAX_MEMORY_CACHE_SIZE, Int.MAX_VALUE, Int.MAX_VALUE
+        ) // Max cache entry size
         configBuilder
-                .setBitmapMemoryCacheParamsSupplier(
-                        () -> bitmapCacheParams)
-                .setMainDiskCacheConfig(DiskCacheConfig.newBuilder(Utils.getApp())
-                                .setBaseDirectoryPath(getExternalCacheDir(context))
-                                .setBaseDirectoryName(IMAGE_PIPELINE_CACHE_DIR)
-                                .setMaxCacheSize(MAX_DISK_CACHE_SIZE)
-                                .build());
+            .setBitmapMemoryCacheParamsSupplier { bitmapCacheParams }
+            .setMainDiskCacheConfig(
+                DiskCacheConfig.newBuilder(Utils.getApp())
+                    .setBaseDirectoryPath(getExternalCacheDir(context))
+                    .setBaseDirectoryName(IMAGE_PIPELINE_CACHE_DIR)
+                    .setMaxCacheSize(MAX_DISK_CACHE_SIZE.toLong())
+                    .build()
+            )
     }
 
-    public static File getExternalCacheDir(final Context context) {
-        if (hasExternalCacheDir())
-            return context.getExternalCacheDir();
+    fun getExternalCacheDir(context: Context): File? {
+        if (hasExternalCacheDir()) return context.externalCacheDir
 
         // Before Froyo we need to construct the external cache dir ourselves
-        final String cacheDir = "/Android/data/" + context.getPackageName() + "/cache/";
-        return createFile(Environment.getExternalStorageDirectory().getPath() + cacheDir, "");
+        val cacheDir = "/Android/data/" + context.packageName + "/cache/"
+        return createFile(Environment.getExternalStorageDirectory().path + cacheDir, "")
     }
 
-    public static boolean hasExternalCacheDir() {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO;
+    fun hasExternalCacheDir(): Boolean {
+        return true
     }
 
-    public static File createFile(String folderPath, String fileName) {
-        File destDir = new File(folderPath);
+    fun createFile(folderPath: String?, fileName: String?): File {
+        val destDir = File(folderPath)
         if (!destDir.exists()) {
-            destDir.mkdirs();
+            destDir.mkdirs()
         }
-        return new File(folderPath, fileName);
+        return File(folderPath, fileName)
     }
 }
