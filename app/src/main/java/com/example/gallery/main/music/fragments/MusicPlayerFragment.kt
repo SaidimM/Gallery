01925d711caad2f5b675cbec054902cbd128ccdd
saidim <1466913327@qq.com>
2023-08-31 17:02:53 +0800

    music: fluid view animation

--- app/src/main/java/com/example/gallery/main/music/fragments/MusicPlayerFragment.kt
+++ app/src/main/java/com/example/gallery/main/music/fragments/MusicPlayerFragment.kt
@@ -1,14 +1,12 @@
 package com.example.gallery.main.music.fragments
 
 import android.graphics.Bitmap
-import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.FragmentContainerView
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import androidx.palette.graphics.Palette
 import com.example.gallery.R
 import com.example.gallery.base.ui.pge.BaseFragment
 import com.example.gallery.base.utils.ViewUtils.loadAlbumCover
@@ -17,7 +15,6 @@ import com.example.gallery.main.music.viewModels.MusicPlayerViewModel
 import com.example.gallery.main.music.viewModels.MusicViewModel
 import com.example.gallery.main.video.player.state.PlayState
 import com.example.gallery.media.local.bean.Music
-import com.facebook.common.media.MediaUtils
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -39,9 +36,10 @@ class MusicPlayerFragment(private val containerView: FragmentContainerView) : Ba
         binding.play.setOnClickListener { state.onPlayPressed() }
         binding.next.setOnClickListener { state.onNextPressed() }
         binding.playerLayout.setOnClickListener {
-            if (behavior.state != BottomSheetBehavior.STATE_EXPANDED) behavior.state = BottomSheetBehavior.STATE_EXPANDED
+            if (behavior.state != BottomSheetBehavior.STATE_EXPANDED) behavior.state =
+                BottomSheetBehavior.STATE_EXPANDED
         }
-        behavior.addBottomSheetCallback(object: BottomSheetBehavior.BottomSheetCallback(){
+        behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
             override fun onStateChanged(bottomSheet: View, newState: Int) {
                 if (newState == BottomSheetBehavior.STATE_COLLAPSED) {
                     binding.playerLayout.animate().alphaBy(0f).alpha(1f).setDuration(500).start()
@@ -59,7 +57,7 @@ class MusicPlayerFragment(private val containerView: FragmentContainerView) : Ba
             var bitmap: Bitmap?
             lifecycleScope.launchWhenCreated {
                 bitmap = withContext(Dispatchers.Main) { loadAlbumCover(it, binding.albumCover) }
-                initPlayerBackground(binding.root, bitmap!!)
+                launch(Dispatchers.Main) { binding.playerBack.initBackground(bitmap!!) }
             }
             binding.musicName.text = it.name
             initPlayDetails(it)
@@ -82,14 +80,8 @@ class MusicPlayerFragment(private val containerView: FragmentContainerView) : Ba
         }
     }
 
-    private fun initPlayerBackground(view: View, bitmap: Bitmap) {
-        val palette = Palette.from(bitmap).generate()
-        val color = view.backgroundTintList?.defaultColor
-        view.background.setTint(palette.getVibrantColor(color ?: Color.WHITE))
-    }
-
     private fun initPlayDetails(music: Music) {
-        lifecycleScope.launch{
+        lifecycleScope.launch {
             state.music.value?.let { loadAlbumCover(music, binding.largeCover) }
         }
         binding.songName.text = music.name
---
+++ app/src/main/java/com/example/gallery/main/music/views/FluidView.kt
@@ -0,0 +1,95 @@
+package com.example.gallery.main.music.views
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
+import android.util.AttributeSet
+import android.view.Gravity
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.FrameLayout
+import androidx.palette.graphics.Palette
+import com.example.gallery.R
+import kotlin.math.sqrt
+
+class FluidView(context: Context, attributeSet: AttributeSet? = null) : FrameLayout(context, attributeSet) {
+    private val TAG = "FluidView"
+
+    private val gradientBottomLayer =
+        GradientDrawable(GradientDrawable.Orientation.BOTTOM_TOP, intArrayOf(Color.WHITE, Color.GRAY))
+
+    private val gradientMiddleLayer =
+        GradientDrawable(GradientDrawable.Orientation.TL_BR, intArrayOf(Color.WHITE, Color.GRAY))
+
+    private val gradientTopLayer =
+        GradientDrawable(GradientDrawable.Orientation.TR_BL, intArrayOf(Color.WHITE, Color.GRAY))
+
+    private val viewBottom = View(context)
+    private val viewMiddle = View(context)
+    private val viewTop = View(context)
+
+    init {
+        addView(viewBottom)
+//        addView(viewMiddle)
+//        addView(viewTop)
+        initLayers()
+    }
+
+    fun initBackground(bitmap: Bitmap) {
+        val palette = Palette.from(bitmap).generate()
+        gradientBottomLayer.colors = intArrayOf(palette.getDominantColor(Color.WHITE), Color.WHITE)
+        gradientMiddleLayer.colors = intArrayOf(palette.getVibrantColor(Color.WHITE), palette.getLightVibrantColor(Color.WHITE))
+        gradientTopLayer.colors = intArrayOf(palette.getDarkVibrantColor(Color.GRAY), palette.getDarkMutedColor(Color.GRAY))
+
+        viewBottom.startAnimation(AnimationUtils.loadAnimation(context, R.anim.anim_fluid_bottom).apply {
+            repeatMode = Animation.RESTART
+            repeatCount = Animation.INFINITE
+        })
+        viewMiddle.startAnimation(AnimationUtils.loadAnimation(context, R.anim.anim_fluid_middle).apply {
+            repeatMode = Animation.RESTART
+            repeatCount = Animation.INFINITE
+        })
+        viewTop.startAnimation(AnimationUtils.loadAnimation(context, R.anim.anim_fluid_top).apply {
+            repeatMode = Animation.REVERSE
+            repeatCount = Animation.INFINITE
+        })
+    }
+
+    private fun initLayers() {
+        val viewBottomRadius = sqrt((width * width + height * height).toDouble()).toInt()
+        (viewBottom.layoutParams as LayoutParams).apply {
+            width = viewBottomRadius
+            height = viewBottomRadius
+        }.apply {
+            background = gradientBottomLayer
+            top = -(viewBottomRadius - height) / 2
+            left = -(viewBottomRadius - width) / 2
+        }
+        val viewMiddleRadius = sqrt((width * width + height * height).toDouble()).toInt() * 3 / 2
+        (viewMiddle.layoutParams as LayoutParams).apply {
+            width = viewMiddleRadius
+            height = viewMiddleRadius
+            gravity = Gravity.CENTER
+        }.apply {
+            background = gradientMiddleLayer
+            top = -(viewMiddleRadius - height) / 2
+            left = -(viewMiddleRadius - width) / 2
+            alpha = 0.4f
+        }
+        viewMiddle.visibility = GONE
+        val viewTopRadius = sqrt((width * width + height * height).toDouble()).toInt()
+        (viewTop.layoutParams as LayoutParams).apply {
+            width = viewTopRadius
+            height = viewTopRadius
+            gravity = Gravity.CENTER
+        }.apply {
+            background = gradientTopLayer
+            top = -(viewBottomRadius - height) / 2
+            left = -(viewBottomRadius - width) / 2
+            alpha = 0.4f
+        }
+        viewTop.visibility = GONE
+    }
+}
