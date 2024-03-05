558530b6953a8c913f85b6c88904a0958d10f04b
saidim <1466913327@qq.com>
2024-03-05 16:28:08 +0800

    [controller] fluid view

--- app/src/main/java/com/example/gallery/main/music/views/FluidView.kt
+++ app/src/main/java/com/example/gallery/main/music/views/FluidView.kt
@@ -3,29 +3,26 @@ package com.example.gallery.main.music.views
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.*
-import android.graphics.drawable.GradientDrawable
 import android.util.AttributeSet
 import android.view.animation.Animation
-import android.view.animation.AnimationUtils
 import android.view.animation.LinearInterpolator
 import android.widget.FrameLayout
 import androidx.core.graphics.withRotation
 import androidx.palette.graphics.Palette
 import com.blankj.utilcode.util.ScreenUtils
-import com.example.gallery.R
-import com.example.gallery.base.utils.ViewUtils.setWidth
 import kotlin.math.sqrt
 
 class FluidView(context: Context, attributeSet: AttributeSet? = null) : FrameLayout(context, attributeSet) {
     private val TAG = "FluidView"
 
-    private val ANIMATION_INTERVAL = 3000
+    private val ANIMATION_INTERVAL = 30_000L
 
     private var palette: Palette? = null
+    private var bitmap: Bitmap? = null
 
     private var rotatedAngle = 0f
     private val animation = ValueAnimator.ofFloat(0f, 360f).apply {
-        duration = ANIMATION_INTERVAL.toLong()
+        duration = ANIMATION_INTERVAL
         repeatCount = Animation.INFINITE
         repeatMode = ValueAnimator.RESTART
         interpolator = LinearInterpolator()
@@ -45,13 +42,14 @@ class FluidView(context: Context, attributeSet: AttributeSet? = null) : FrameLay
     fun initBackground(bitmap: Bitmap) {
         palette = Palette.from(bitmap).generate()
         animation.start()
+        this.bitmap = bitmap
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         drawRectBottom(canvas)
-//        drawRectMiddle()
-//        drawRectTop()
+        drawRectMiddle(canvas)
+        drawRectTop(canvas)
     }
 
     private fun drawRectBottom(canvas: Canvas) {
@@ -59,45 +57,42 @@ class FluidView(context: Context, attributeSet: AttributeSet? = null) : FrameLay
         val top = -(radius - height) / 2
         val rect = Rect(left, top, radius, radius)
         val paint = Paint().apply {
-            shader = LinearGradient(0f, 0f, radius.toFloat(), radius.toFloat(), intArrayOf(
-                    palette?.getDominantColor(Color.WHITE) ?: Color.WHITE, Color.WHITE
+            shader = LinearGradient(
+                0f, 0f, radius.toFloat(), radius.toFloat(), intArrayOf(
+                    palette?.getDominantColor(Color.WHITE) ?: Color.WHITE, palette?.getVibrantColor(Color.WHITE)?: Color.WHITE
                 ), null, Shader.TileMode.CLAMP
             )
         }
         canvas.withRotation(rotatedAngle, (width / 2).toFloat(), (height / 2).toFloat()) { drawRect(rect, paint) }
     }
 
-    private fun drawRectTop() {
-        val canvas = Canvas()
-        val rect = Rect(radius, radius, radius, radius).apply {
-            left = -(radius - width) / 2
-            top = -(radius - height) / 2
-        }
+    private fun drawRectTop(canvas: Canvas) {
+        val left = -(radius - width) / 2
+        val top = -(radius - height) / 2
+        val rect = Rect(left, top, radius, radius)
         val paint = Paint().apply {
             shader = LinearGradient(
-                0f, 0f, 0f, radius.toFloat(), intArrayOf(
-                    palette?.getLightVibrantColor(Color.WHITE)?: Color.WHITE, palette?.getDarkVibrantColor(Color.BLACK)?: Color.BLACK
+                0f, 0f, radius.toFloat(), radius.toFloat(), intArrayOf(
+                    palette?.getDarkMutedColor(Color.BLACK) ?: Color.WHITE,
+                    palette?.getDarkVibrantColor(Color.BLACK) ?: Color.BLACK
                 ), null, Shader.TileMode.CLAMP
             )
             blendMode = BlendMode.DST_ATOP
         }
-        canvas.withRotation(rotatedAngle, 0.5f, 0.5f) { drawRect(rect, paint) }
+        canvas.withRotation(rotatedAngle, (width / 2).toFloat(), (height / 2).toFloat()) { drawRect(rect, paint) }
     }
 
-    private fun drawRectMiddle() {
-        val canvas = Canvas()
-        val rect = Rect(radius, radius, radius, radius).apply {
-            left = -(radius - width) / 2
-            top = -(radius - height) / 2
-        }
+    private fun drawRectMiddle(canvas: Canvas) {
+        val left = -(radius - width) / 2
+        val top = -(radius - height) / 2
+        val rect = Rect(left, top, radius, radius)
         val paint = Paint().apply {
             shader = LinearGradient(
-                0f, 0f, 0f, radius.toFloat(), intArrayOf(
-                    Color.WHITE, Color.TRANSPARENT
-                ), null, Shader.TileMode.CLAMP
+                0f, 0f, radius.toFloat(), radius.toFloat(), intArrayOf(Color.WHITE, Color.TRANSPARENT),
+                null, Shader.TileMode.CLAMP
             )
             blendMode = BlendMode.DST_OUT
         }
-        canvas.withRotation(-rotatedAngle, 0.5f, 0.5f) { drawRect(rect, paint) }
+        canvas.withRotation(-rotatedAngle, (width / 2).toFloat(), (height / 2).toFloat()) { drawRect(rect, paint) }
     }
 }
