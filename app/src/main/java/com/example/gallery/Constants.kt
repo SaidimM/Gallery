4fbdac5ac3c4e6e733adf391fe0e9774fd7f3d0c
saidim <1466913327@qq.com>
2024-03-25 13:49:10 +0800

    change gradle version

--- app/src/main/java/com/example/gallery/Constants.kt
+++ app/src/main/java/com/example/gallery/Constants.kt
@@ -5,6 +5,11 @@ import com.example.gallery.main.music.views.EaseCubicInterpolator
 import com.example.gallery.media.music.local.bean.Music
 
 object Constants {
+    val NOTIFICATION_CHANNEL_ID: String = "NOTIFICATION_CHANNEL_ID"
+    val CLOSE_ACTION: String = "CLOSE_ACTION"
+    val PRE_ACTION: String = "PRE_ACTION"
+    val NEXT_ACTION: String = "NEXT_ACTION"
+    val PLAY_PAUES_ACTION: String = "PLAY_PAUES_ACTION"
     val ALBUM_COVER_DIR = Utils.getApp().getExternalFilesDir("")!!.absolutePath + "/album/"
     val LYRIC_DIR = Utils.getApp().getExternalFilesDir("")!!.absolutePath + "/lyric/"
 
